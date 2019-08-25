8723f479ac748f703fe8acda39977663f9f4b2d8
naouezuki <nao6up@gmail.com>
2019-08-25 14:44:05 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
+  get  'tweets'     => 'tweets#index'
+  get  'tweets/new' => 'tweets#new'
+  post 'tweets'     => 'tweets#create'
+  delete  'tweets/:id'  => 'tweets#destroy'
+  patch   'tweets/:id'  => 'tweets#update'
+  get   'tweets/:id/edit'  => 'tweets#edit'
+  get 'tweets/:id' => 'tweets#show'
+  get  'users/:id'  => 'users#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
